3e849b5744ba71113d31d471ee83e7c027127018
Philip Njuguna <philkiangai@gmail.com>
2021-08-24 16:11:07 +0300

    -udated ads code

--- Modules/Blog/Resources/views/show.blade.php
+++ Modules/Blog/Resources/views/show.blade.php
@@ -18,17 +18,6 @@
         </div>
         <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
 
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8438942375783163"
-                    crossorigin="anonymous"></script>
-            <ins class="adsbygoogle"
-                 style="display:block; text-align:center;"
-                 data-ad-layout="in-article"
-                 data-ad-format="fluid"
-                 data-ad-client="ca-pub-8438942375783163"
-                 data-ad-slot="9183144582"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
 
 
             {!! $blog->content !!}
@@ -36,6 +25,20 @@
 
         </div>
     </div>
+
+
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8438942375783163"
+            crossorigin="anonymous"></script>
+    <ins class="adsbygoogle"
+         style="display:block; text-align:center;"
+         data-ad-layout="in-article"
+         data-ad-format="fluid"
+         data-ad-client="ca-pub-8438942375783163"
+         data-ad-slot="9183144582"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
 
 </x-guest-layout>
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,9 +16,21 @@
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8438942375783163"
+            crossorigin="anonymous"></script>
 
 
-
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8438942375783163"
+            crossorigin="anonymous"></script>
+    <ins class="adsbygoogle"
+         style="display:block; text-align:center;"
+         data-ad-layout="in-article"
+         data-ad-format="fluid"
+         data-ad-client="ca-pub-8438942375783163"
+         data-ad-slot="9183144582"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 
     @livewireStyles
 </head>
