e041c8211f02b3eb60d5256e3cdc3e2dd555eaad
Philip Njuguna <philkiangai@gmail.com>
2021-08-24 15:49:35 +0300

    Update privacy-policy.blade.php

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -1,11 +1,5 @@
 <x-guest-layout>
     <div class="relative px-4 sm:px-6 lg:px-8 mt-12">
-        <div class="text-lg max-w-prose mx-auto">
-            <h1>
-                <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">{{ $blog->title }}</span>
-                <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">{{ $blog->title }}</span>
-            </h1>
-        </div>
         <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
 
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8438942375783163"
@@ -108,5 +102,5 @@
 
         </div>
     </div>
-    
+
 </x-guest-layout>
