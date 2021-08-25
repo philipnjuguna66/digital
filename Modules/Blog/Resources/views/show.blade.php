74007c97601bc314586ddd539c2b552abbd7bdef
Philip Njuguna <philkiangai@gmail.com>
2021-08-25 11:38:04 +0300

    Update show.blade.php

--- Modules/Blog/Resources/views/show.blade.php
+++ Modules/Blog/Resources/views/show.blade.php
@@ -19,25 +19,37 @@
         <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
 
 
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8438942375783163"
+                    crossorigin="anonymous"></script>
+            <ins class="adsbygoogle"
+                 style="display:block; text-align:center;"
+                 data-ad-layout="in-article"
+                 data-ad-format="fluid"
+                 data-ad-client="ca-pub-8438942375783163"
+                 data-ad-slot="4381467362"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
 
             {!! $blog->content !!}
 
 
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8438942375783163"
+                    crossorigin="anonymous"></script>
+            <ins class="adsbygoogle"
+                 style="display:block; text-align:center;"
+                 data-ad-layout="in-article"
+                 data-ad-format="fluid"
+                 data-ad-client="ca-pub-8438942375783163"
+                 data-ad-slot="4381467362"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+            
         </div>
     </div>
 
 
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8438942375783163"
-            crossorigin="anonymous"></script>
-    <ins class="adsbygoogle"
-         style="display:block; text-align:center;"
-         data-ad-layout="in-article"
-         data-ad-format="fluid"
-         data-ad-client="ca-pub-8438942375783163"
-         data-ad-slot="9183144582"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
 
 
 </x-guest-layout>
