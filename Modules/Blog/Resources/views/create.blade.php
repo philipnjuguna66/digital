0577c1178336621aa89139bfa8f22e751b99128f
philipnjuguna66 <philipnjuguna66@gmail.com>
2021-11-06 11:52:56 +0300

    Update create.blade.php

--- Modules/Blog/Resources/views/create.blade.php
+++ Modules/Blog/Resources/views/create.blade.php
@@ -1,9 +1,11 @@
 <x-app-layout>
 
-
+    @push('page_css')
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @endpush
 
     @section('content')
-        <livewire:create-blog />
+        <livewire:create-blog/>
 
     @endsection
 
