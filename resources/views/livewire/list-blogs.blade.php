67051c19073fe157fc146c25bceb40eed537087d
philipnjuguna66 <philipnjuguna66@gmail.com>
2021-11-06 11:59:23 +0300

    ssd

--- resources/views/livewire/list-blogs.blade.php
+++ resources/views/livewire/list-blogs.blade.php
@@ -49,7 +49,7 @@
                     </td>
 
                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <a href="{{ route('manage.blog.edit') }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                        <a href="{{ route('manage.blog.edit', $blogs) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
                     </td>
                 </tr>
             @endforeach
