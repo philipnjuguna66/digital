6d5d0d65eb1801543b3aee1b03145061e6b95718
philipnjuguna66 <philipnjuguna66@gmail.com>
2021-11-09 19:53:24 +0300

    -blogs

--- app/Http/Resources/BlogResource.php
+++ app/Http/Resources/BlogResource.php
@@ -21,7 +21,7 @@ class BlogResource extends JsonResource
             'meta_title' => $this->meta_title,
             'meta_description' => $this->meta_description,
             'content' => $this->content,
-            'featured_image' => ! is_null($this->featured_image) ? url('storage/'.$this->featured_image) : null,
+            'featured_image' => $this->featured_image,
             'is_published' => $this->is_published,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
