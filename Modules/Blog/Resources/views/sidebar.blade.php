63d51aa5f142b66f7e3cb5ed97ab51b368e1af4c
philipnjuguna66 <philipnjuguna66@gmail.com>
2021-11-09 19:59:32 +0300

    -blogs

--- Modules/Blog/Resources/views/sidebar.blade.php
+++ Modules/Blog/Resources/views/sidebar.blade.php
@@ -39,21 +39,21 @@
     <div class="widget">
         <h6 class="widget-title">Popular Posts</h6>
         <div class="media mb-3 border-bottom pb-3 border-color-extra-light-gray">
-            <img class="me-3 border-radius-4" src="img/blog/insta-01.jpg" alt="...">
+            <img class="me-3 border-radius-4" src="{{ asset('img/blog/insta-01.jpg') }}" alt="...">
             <div class="media-body align-self-center">
                 <h4 class="h6 mb-1"><a href="#!">Analysis tips you need to learn now.</a></h4>
                 <a href="#!" class="small"><i class="far fa-calendar-check me-2"></i>Feb. 05, 2021</a>
             </div>
         </div>
         <div class="media mb-3 border-bottom pb-3 border-color-extra-light-gray">
-            <img class="me-3 border-radius-4" src="img/blog/insta-02.jpg" alt="...">
+            <img class="me-3 border-radius-4" src="{{ asset('img/blog/insta-02.jpg') }}" alt="...">
             <div class="media-body align-self-center">
                 <h4 class="h6 mb-1"><a href="#!">Instructions to choose the right SEO agency.</a></h4>
                 <a href="#!" class="small"><i class="far fa-calendar-check me-2"></i>Jan. 28, 2021</a>
             </div>
         </div>
         <div class="media">
-            <img class="me-3 border-radius-4" src="img/blog/insta-03.jpg" alt="...">
+            <img class="me-3 border-radius-4" src="{{ asset('img/blog/insta-03.jpg') }}" alt="...">
             <div class="media-body align-self-center">
                 <h4 class="h6 mb-1"><a href="#!">How to grow your business with SEo.</a></h4>
                 <a href="#!" class="small"><i class="far fa-calendar-check me-2"></i>Jan. 18, 2021</a>
