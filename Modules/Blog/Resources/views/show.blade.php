467b442d005a79ded1a49c5d614f1dbd671b9dae
philipnjuguna66 <philipnjuguna66@gmail.com>
2021-12-16 13:59:20 +0300

    added discus commenting system

--- Modules/Blog/Resources/views/show.blade.php
+++ Modules/Blog/Resources/views/show.blade.php
@@ -213,7 +213,5 @@
             })();
         </script>
         <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-        Copy
-
     @endpush
 </x-app-layout>
